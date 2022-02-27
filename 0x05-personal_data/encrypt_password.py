52e963c2a6357548b85e5fb7909956fc5fb139d1
ramii02 <louhibirami11@gmail.com>
2022-02-27 16:04:58 +0100

    done

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+encrypt_password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    hash_password function
+    """
+    password = bytes(password, 'utf-8')
+    return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid function
+    """
